089d76aef47c7bde1650bb1d212df5b4be2ab2c2
6q5so95 <103797420+6q5so95@users.noreply.github.com>
2023-12-23 16:52:29 +0900

    Create conftest.py

---
+++ poetry_test/test/conftest.py
@@ -0,0 +1,27 @@
+# conftest.py
+
+# テストパッケージ毎に出力ファイル名変更が可能なfixture
+# テストパッケージ毎に配置
+
+# 使い方
+# test_xxx.py
+
+# conftest.pyのfixtureを呼び出し
+# def test_func(change_log_file):
+#    logger = logging.getLogger("test_logger") 
+#    logger.info("テストログ")
+
+
+import pytest
+import logging
+
+@pytest.fixture(scope="function")
+def change_log_file():
+    logger = logging.getLogger("test_logger")
+    old_handler = logger.handlers[0]
+    new_handler = logging.FileHandler("new.log")
+    logger.addHandler(new_handler)
+    logger.removeHandler(old_handler)
+    yield 
+    logger.addHandler(old_handler)
+    logger.removeHandler(new_handler)
